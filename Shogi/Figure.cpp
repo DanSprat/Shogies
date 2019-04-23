f1ae5d364fe7736123176594ba3c5591356ddb9d
DanSprat <49001534+DanSprat@users.noreply.github.com>
2019-04-23 09:17:49 +0300

    Update Figure.cpp

--- Shogi/Figure.cpp
+++ Shogi/Figure.cpp
@@ -1,13 +1,13 @@
 #include "Figure.h"
 
 
-Figures::Figures(float x, float y, int side, String FS, String FT, bool HaveTransform)
+Figures::Figures(int x, int y, int side, String FS, String FT, bool HaveTransform)
 {
-
+	RulesMove = new VectorMove[1];
 	roots[10][10];
 	memset(roots, 0, sizeof(roots));
 	isMove = false; isClicked = false;
-	float X, Y;
+	int X, Y;
 	X = x;
 	Y = y;
 	transformation = 0;
@@ -122,12 +122,12 @@ Figures* SelectedFigure(int &turn, Figures* FiguresBlack[], Figures* FiguresWhit
 	}
 
 }
-King::King(float x, float y, int side, String FS)
+King::King(int x, int y, int side, String FS)
 {
 	roots[10][10];
 	memset(roots, 0, sizeof(roots));
 	isMove = false; isClicked = false;
-	float X, Y;
+	int X, Y;
 	X = x;
 	Y = y;
 	transformation = 0;
@@ -145,7 +145,7 @@ King::King(float x, float y, int side, String FS)
 	SpriteD.setTextureRect(IntRect(33, 709, 55, 65));//В дальнейшем координаты x,y будут перемножаться на 85  и размеры его будут 85 на 85 p.s Разобраться с картинкой
 	SpriteD.setPosition(1235, 815);// тут тоже все будет зависеть от координат  p.s Разобраться с картинкой
 }
-Pawn::Pawn(float x, float y, int side, String FS, String FT)
+Pawn::Pawn(int x, int y, int side, String FS, String FT)
 {
 	RulesMove = new VectorMove[1];
 	if (side == 1)
@@ -173,7 +173,7 @@ Pawn::Pawn(float x, float y, int side, String FS, String FT)
 	roots[10][10];
 	memset(roots, 0, sizeof(roots));
 	isMove = false; isClicked = false;
-	float X, Y;
+	int X, Y;
 	X = x;
 	Y = y;
 	transformation = 0;
@@ -212,7 +212,7 @@ VectorMove Figures::getRulesMove(int i)
 {
 	return RulesMove[i];
 }
-Rook::Rook(float x, float y, int side, String FS, String FT)
+Rook::Rook(int x, int y, int side, String FS, String FT)
 {
 	RulesMove = new VectorMove[32];
 	for (int i = 1; i < 9; i++)
@@ -244,7 +244,7 @@ Rook::Rook(float x, float y, int side, String FS, String FT)
 	roots[10][10];
 	memset(roots, 0, sizeof(roots));
 	isMove = false; isClicked = false;
-	float X, Y;
+	int X, Y;
 	X = x;
 	Y = y;
 	transformation = 0;
@@ -276,7 +276,7 @@ Rook::Rook(float x, float y, int side, String FS, String FT)
 	
 }
 
-Knight::Knight(float x, float y, int side, String FS, String FT)
+Knight::Knight(int x, int y, int side, String FS, String FT)
 {
 	RulesMove = new VectorMove[32];
 	for (int i = 1; i < 9; i++)
@@ -308,7 +308,7 @@ Knight::Knight(float x, float y, int side, String FS, String FT)
 	roots[10][10];
 	memset(roots, 0, sizeof(roots));
 	isMove = false; isClicked = false;
-	float X, Y;
+	int X, Y;
 	X = x;
 	Y = y;
 	transformation = 0;
@@ -339,7 +339,7 @@ Knight::Knight(float x, float y, int side, String FS, String FT)
 }
 
 
-Arrow::Arrow(float x, float y, int side, String FS, String FT)
+Arrow::Arrow(int x, int y, int side, String FS, String FT)
 {
 	RulesMove = new VectorMove[16];
 	for (int i = 1; i < 9; i++)
@@ -372,7 +372,7 @@ Arrow::Arrow(float x, float y, int side, String FS, String FT)
 	roots[10][10];
 	memset(roots, 0, sizeof(roots));
 	isMove = false; isClicked = false;
-	float X, Y;
+	int X, Y;
 	X = x;
 	Y = y;
 	transformation = 0;
@@ -402,7 +402,7 @@ Arrow::Arrow(float x, float y, int side, String FS, String FT)
 	///////////////////////////////////////////////////////////////////////////////////////////////////
 
 }
-Horse::Horse(float x, float y, int side, String FS, String FT)
+Horse::Horse(int x, int y, int side, String FS, String FT)
 {
 	RulesMove = new VectorMove[2];
 	if (side == 1)
@@ -432,7 +432,7 @@ Horse::Horse(float x, float y, int side, String FS, String FT)
 	roots[10][10];
 	memset(roots, 0, sizeof(roots));
 	isMove = false; isClicked = false;
-	float X, Y;
+	int X, Y;
 	X = x;
 	Y = y;
 	transformation = 0;
@@ -461,7 +461,7 @@ Horse::Horse(float x, float y, int side, String FS, String FT)
 
 	///////////////////////////////////////////////////////////////////////////////////////////////////
 }
- Gold::Gold(float x, float y, int side, String FS) {
+ Gold::Gold(int x, int y, int side, String FS) {
 	 RulesMove = new VectorMove[6];
 	 if (side == 1)
 	 {
@@ -485,7 +485,7 @@ Horse::Horse(float x, float y, int side, String FS, String FT)
 	 roots[10][10];
 	 memset(roots, 0, sizeof(roots));
 	 isMove = false; isClicked = false;
-	 float X, Y;
+	 int X, Y;
 	 X = x;
 	 Y = y;
 	 transformation = 0;
@@ -503,7 +503,7 @@ Horse::Horse(float x, float y, int side, String FS, String FT)
 	 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
  }
- Silver::Silver(float x, float y, int side, String FS)
+ Silver::Silver(int x, int y, int side, String FS)
  {
 	 RulesMove = new VectorMove[5];
 	 if (side == 1)
@@ -527,7 +527,7 @@ Horse::Horse(float x, float y, int side, String FS, String FT)
 	 roots[10][10];
 	 memset(roots, 0, sizeof(roots));
 	 isMove = false; isClicked = false;
-	 float X, Y;
+	 int X, Y;
 	 X = x;
 	 Y = y;
 	 transformation = 0;
@@ -544,8 +544,80 @@ Horse::Horse(float x, float y, int side, String FS, String FT)
 
 	 ///////////////////////////////////////////////////////////////////////////////////////////////////
  }
- int Figures::SearchRoots(Figures a)
+ void Figures::SearchRoots(int array[10][10],Figures& pa)
  {
-	 int k = sizeof(RulesMove);
- }
+	 VectorMove StopPos[8];
+	 for (int i = 0; i < 8; i++)
+	 {
+		 StopPos[i].getCompX() = 0;
+		 StopPos[i].getCompY() = 0;
 
+	 }
+	 int i = 0;
+	 int  tempX=0;
+	 int tempY=0;
+	 for (i = 0; i < sizeof(RulesMove); i++)
+	 {
+		 tempX = x + RulesMove[i].getScaleCompX();
+		 tempY = y + RulesMove[i].getScaleCompY();
+		 if ((tempX<10)&&(tempX>0)&&(tempY<10)&&(tempY>0))
+		 {
+			 if (sizeof(RulesMove) > 10)
+			 {
+				 if ((array[tempX][tempY]!=side) && (CheckCheck(pa, tempX, tempY) == 1))
+				 {
+					 roots[tempX][tempY] = 1;
+					 if (array[tempX][tempY] == 0)
+					 { }
+					 else
+					 {
+						 StopPos[i++] = {tempX,tempY};
+					 }
+				 }
+				 else
+				 {
+					 roots[tempX][tempY] = 0;
+					 StopPos[i++] = { tempX,tempY };
+				 }
+			 }
+			 else if ((array[tempX][tempY] != side) && (CheckCheck(pa, tempX, tempY) == 1))
+			 {
+				 roots[tempX][tempY] = 1;
+			 }
+			 else
+			 {
+				 roots[tempX][tempY] = 0;
+			 }
+
+		 }
+	}
+	 for (i = 0; i < 8; i++)
+	 {
+		 int j = 1;
+		 int temp2X = 0;
+		 int temp2Y = 0;
+		 if (StopPos[i].getCompX() > StopPos[i].getCompY())
+		 {
+			 temp2X = (StopPos[i].getCompX()) / abs(StopPos[i].getCompX());
+			 temp2Y= (StopPos[i].getCompY()) / abs(StopPos[i].getCompX());
+		 }
+		 else
+		 {
+			 temp2X = (StopPos[i].getCompX()) / abs(StopPos[i].getCompY());
+			 temp2Y = (StopPos[i].getCompY()) / abs(StopPos[i].getCompY());
+		 }
+		 while (((StopPos[i].getCompX()+temp2X*j) < 10) && ((StopPos[i].getCompX() + temp2X * j) > 0) && ((StopPos[i].getCompY() + temp2Y * j) < 10) && ((StopPos[i].getCompY() + temp2Y * j > 0)))
+		 {
+			 StopPos[i].getCompX() += temp2X*(j);
+			 StopPos[i].getCompY() += temp2Y*(j);
+             roots[StopPos[i].getCompX()][StopPos[i].getCompY()] = 0;
+			 j++;
+
+		 }
+	 }
+ }
+ bool CheckCheck(Figures &a, int NewX, int NewY)
+ {
+	 return true;
+ }
+ 

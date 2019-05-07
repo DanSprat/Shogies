c0d57e1a93124443d2e9a409934223702ba8f5a8
DanSprat <49001534+DanSprat@users.noreply.github.com>
2019-05-07 08:22:24 +0300

    Add files via upload

---
+++ Shogi/map.h
@@ -0,0 +1,17 @@
+using namespace sf;
+#include <SFML\Graphics.hpp>
+const int HEIGHT_MAP = 10;//размер карты высота
+const int WIDTH_MAP = 10;//размер карты ширина
+String TileMap[HEIGHT_MAP] = {
+"0000000000",
+"0         ",
+"0         ",
+"0         ",
+"0         ",
+"0         ",
+"0         ",
+"0         ",
+"0         ",
+"0         ",
+};
+
